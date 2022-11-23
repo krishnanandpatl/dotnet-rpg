11445d30e4327f39d1c6101a6f41a6c85a6d1146
Krishna Nand Patel <imkrishna8737@gmail.com>
2022-11-23 18:03:37 +0530

    Before AutoMapper

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,18 +20,18 @@ namespace dotnet_rpg.Controllers
         }
         [HttpGet]
         [Route("GetAll")] //instead of route we could have directly used [HttpGet("GetAll)] would have worked fine
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
         {
             return Ok(await this.characterService.GetAllCharacters());
         }
         //we want now a single character from the list
         [HttpGet("{id}")]
-        public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id)
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int id)
         {
             return Ok(await this.characterService.GetCharacterById(id));
         }
         [HttpPost]
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
         {
             return Ok(await this.characterService.AddCharacter(newCharacter));
         }
