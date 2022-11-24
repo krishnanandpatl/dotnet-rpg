72a5ca4750485770215ec6ae931f0f19121ed708
Krishna Nand Patel <imkrishna8737@gmail.com>
2022-11-24 17:31:07 +0530

    HttpPut used

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -12,28 +12,38 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {   
-        private readonly ICharacterService characterService;
+        private readonly ICharacterService _characterService;
         public CharacterController(ICharacterService characterService)
         {
-            this.characterService = characterService;
+            _characterService = characterService;
             
         }
         [HttpGet]
         [Route("GetAll")] //instead of route we could have directly used [HttpGet("GetAll)] would have worked fine
         public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
         {
-            return Ok(await this.characterService.GetAllCharacters());
+            return Ok(await _characterService.GetAllCharacters());
         }
         //we want now a single character from the list
         [HttpGet("{id}")]
         public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int id)
         {
-            return Ok(await this.characterService.GetCharacterById(id));
+            return Ok(await _characterService.GetCharacterById(id));
         }
         [HttpPost]
         public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
         {
-            return Ok(await this.characterService.AddCharacter(newCharacter));
+            return Ok(await _characterService.AddCharacter(newCharacter));
+        }
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
+        {
+            var response=await _characterService.UpdateCharacter(updatedCharacter);
+            if(response.Data==null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
         }
     }
 }
