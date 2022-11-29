788d55d506100f67a5e5f6d3e0423c054858be00
Krishna Nand Patel <imkrishna8737@gmail.com>
2022-11-29 13:19:28 +0530

    REgistration complete

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Data;
+using dotnet_rpg.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] //name of the route is defined by whatever written before of Controller in this case auth.
+    public class AuthController: ControllerBase
+    {
+        private readonly IAuthRepository _authoRepo;
+
+        public AuthController(IAuthRepository authoRepo)
+        {
+            _authoRepo = authoRepo;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response= await _authoRepo.Register(
+                new User {Name=request.Name},request.Password
+            );
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
