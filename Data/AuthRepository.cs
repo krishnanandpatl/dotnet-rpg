b9b88b7be3119ab536c85c5c67eb3a95438d349b
Krishna Nand Patel <imkrishna8737@gmail.com>
2022-11-29 12:27:01 +0530

    register user done

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -7,19 +7,41 @@ namespace dotnet_rpg.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
         public Task<ServiceResponse<string>> Login(string username, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash=passwordHash;
+            user.PasswordSalt=passwordSalt;
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            ServiceResponse<int> response=new ServiceResponse<int>();
+            response.Data=user.Id;
+            return response;
+            //before registering new user user already exists is checked
         }
 
         public Task<bool> UserExists(string username)
         {
             throw new NotImplementedException();
+        }
+        private void CreatePasswordHash(string password,out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac= new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt=hmac.Key;
+                passwordHash=hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
